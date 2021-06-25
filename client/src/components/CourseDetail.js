7a2cab757c9854659be6dee1d6deba4731b55acb
jonhayjr <jonhayjr@gmail.com>
2021-06-24 22:20:31 -0700

    Added loading indicator to CourseDetail component.  Updated description and materialsNeeded logic

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -7,24 +7,25 @@ import axios from 'axios';
 const CourseDetail = (props) => {
        //Create State
        const [course, setCourse] = useState([]);
-       const [user, setUser] = useState([]);
-       const [description, setDescription] = useState([]);
+       const [isLoading, setIsLoading] = useState(true);
+   
 
        //Function to grab API data
        const getCourse = (id) => {
+        //Set isLoading to true
+        setIsLoading(true);
        //Gets data from courses api
          axios.get(`http://localhost:5000/api/courses/${id}`)
          .then(res => {
            //Store course in state
            setCourse(res.data);
-           //Store user in state
-           setUser(res.data.User);
+           console.log(res.data)
 
-        //Create description paragraphs
-        let description = res.data.description.split(/\r?\n/)
-       .filter(desc => desc !== '')
-           setDescription(description)
-       });
+        })
+        .finally(() => {
+            //Set isLoading to False
+            setIsLoading(false);
+        })
        }
        
        //Gets data on page render
@@ -32,23 +33,39 @@ const CourseDetail = (props) => {
         //Get id from url parameter
         const id = props.match.params.id
 
-
         //Get course data based on id
          getCourse(id);
        }, [])
 
+       //Parse description element and create separate paragraphs
+       const description = course.description 
+                ? course.description.split(/\r?\n/)
+                    .filter(desc => desc !== '')
+                    .map((desc, index) => <p key={index}>{desc}</p>)
+                : '';
+       
+       //Parse materialsNeeded and created separate list items
+       const materialsNeeded =   course.materialsNeeded
+       ? course.materialsNeeded.split(/\r?\n/)
+           .filter(material => material !== '')
+           .map((material, index) => <li key={index}>{material.replaceAll('* ', '')}</li>)
+       : '';
 
     return (
         <div className="wrap">
             <h2>Course Detail</h2>
             <form>
-                <div className="main--flex">
+            {
+                isLoading
+                ? <p>Loading...</p>
+                :
+                (<div className="main--flex">
                     <div>
                         <h3 className="course--detail--title">Course</h3>
                         <h4 className="course--name">{course.title}</h4>
-                        <p>{user.firstName} {user.lastName}</p>
-                        {
-                        description.map(desc => <p>{desc}</p>)
+                        <p>By {course.User.firstName} {course.User.lastName}</p>
+                        { 
+                            description
                         }
                     </div>
                     <div>
@@ -57,19 +74,14 @@ const CourseDetail = (props) => {
 
                         <h3 className="course--detail--title">Materials Needed</h3>
                             <ul className="course--detail--list">
-                                <li>1/2 x 3/4 inch parting strip</li>
-                                <li>1 x 2 common pine</li>
-                                <li>1 x 4 common pine</li>
-                                <li>1 x 10 common pine</li>
-                                <li>1/4 inch thick lauan plywood</li>
-                                <li>Finishing Nails</li>
-                                <li>Sandpaper</li>
-                                <li>Wood Glue</li>
-                                <li>Wood Filler</li>
-                                <li>Minwax Oil Based Polyurethane</li>
+                                {
+                                    materialsNeeded
+                                }
                             </ul>
                         </div>
-                </div>
+                </div>)
+            }
+                
             </form>     
         </div>
     )
