3c53db74d7392fd398474bd4849b76002d090471
jonhayjr <jonhayjr@gmail.com>
2021-06-25 07:06:38 -0700

    Created Header component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
 
 //Import components
+import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 
@@ -11,13 +12,14 @@ const App = () => {
 
   return (
     <div>
-    <BrowserRouter>
-      <Switch>
-          <Route exact path="/" component={Courses}/>
-          <Route exact path="/Courses" component={Courses}/>
-          <Route path="/Courses/:id" component={CourseDetail}/>
-      </Switch>
-    </BrowserRouter>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Courses}/>
+            <Route exact path="/Courses" component={Courses}/>
+            <Route path="/Courses/:id" component={CourseDetail}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div class="wrap header--flex">
+                <h1 class="header--logo"><a href="/">Courses</a></h1>
+                <nav>
+                    <ul class="header--signedout">
+                        <li><a href="/signup">Sign Up</a></li>
+                        <li><a href="/signin">Sign In</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
